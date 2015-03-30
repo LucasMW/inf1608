414a6272abce4f126d5d4f8d54fda75afd028e91
LucasMW <great-lucas@hotmail.com>
2015-03-29 22:21:49 -0300

    Debug comment corrected

--- lab3/source.c
+++ lab3/source.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-//#define DEBUG uncomment this line if you wish to see computing proccess 
+#define DEBUG //uncomment this line if you wish to see computing proccess 
 
 /*
 Newton-Rapson
