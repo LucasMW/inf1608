9055c86d0ad863076a0e91c8b84b25444128785a
LucasMW <great-lucas@hotmail.com>
2015-04-02 09:50:19 -0300

    It seens to work

--- lab4/source.c
+++ lab4/source.c
@@ -5,23 +5,50 @@ static double MElement(double* A,int i, int j,int n)
 {
 	return *(A+(i*n+j));
 }
+static double VPrint(double* V, int n)
+{
+	int i;
+	printf("{ ");
+	for(i=0;i<n;i++)
+		printf("%lf ",V[i]);
+	printf("}\n");
+}
+
 static int MIndex(int i, int j,int n)
 {
 	return i*n+j;
+}
+static double MPrint(double* A,int n)
+{
+	int i,j;
+
+	for(i=0;i<n;i++)
+	{	
+		printf("|\t");
+		for(j=0;j<n;j++)
+		{
+			printf("%lf\t",A[MIndex(i,j,n)]);
+		}
+		printf("|\n");
+		
+	}
+	printf("\n");
 }
 void NaiveGaussElimination(int n, double* A, double* b)
 {
 	//Ax=b : A is n*n Matrix
 	int i,j,k;
 	double f;
-	for(j=0; j<n-1;j++)
+	for(j=0; j<n;j++)
 	{
 		for(i=j+1; i<n; i++)
 		{
 			f=MElement(A,i,j,n) / MElement(A,j,j,n);
-			for(k=j+1; k<=n; k++)
+			printf("f: %lf\n",f);
+			for(k=j; k<n; k++)
 			{
 				A[MIndex(i,k,n)]-=f*A[MIndex(j,k,n)];
+				MPrint(A,n);
 			}
 			b[i]-=f*b[j];
 		}
@@ -32,24 +59,26 @@ void BackSubstitution(int n, double* A, double* b, double* x)
 {
 	int i,j;
 	double s;
-	for(i=n-1;i>0;i--)
+	for(i=n-1;i>=0;i--)
 	{
 		s=0;
-		for(j=i+1;j<n+1;j++)
+		for(j=i;j<n;j++)
 		{
 			s+=A[MIndex(i,j,n)]*x[j];
+			printf("s:%lf j:%d i:%d\n",s,j,i);
 		}
-		x[i]=(b[i]-s)/A[MIndex(i,j,n)];
+		x[i]=(b[i]-s)/A[MIndex(i,i,n)];
+		VPrint(x,n);
 	}
 }
 int main (void)
 {
 	double M1[3*3]={1,2,-1,2,1,-2,-3,1,1};
-	double xV1[3];
+	double xV1[3]={0,0,0};
 	double b1[3]={3,3,-6};
 	double M2[6*6]=
 	{	3,  -1,   0,  0,  0,   0.5,
-	   -1,	 3,  -1,  0,  0.5, 0,
+	   	-1,  3,  -1,  0,  0.5, 0,
 		0,  -1,   3, -1,  0,   0,
 		0,   0,  -1,  3, -1,   0,
 		0,   0.5, 0, -1,  3,  -1,
@@ -61,10 +90,11 @@ int main (void)
 	
 	printf("part1\n");
 	NaiveGaussElimination(3,M1,b1);
+	printf("subs\n");
 	BackSubstitution(3,M1,b1,xV1);
 	for(i=0;i<3;i++)
 	{
-		printf("%16g",xV1[i]);
+		printf("%16g\n",xV1[i]);
 	}
 	
 
