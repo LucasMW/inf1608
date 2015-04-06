4575a89735d68ab800c6d2ff9cdda36e709940a1
LucasMW <great-lucas@hotmail.com>
2015-04-05 23:37:29 -0300

    Last try today

--- lab4/source.c
+++ lab4/source.c
@@ -6,8 +6,9 @@ static void MCopy(double * org, double* dest,int n)
 {
 	int i,n2;
 	n2=n*n;
-	for(i=0;i<n;i++)
+	for(i=0;i<n2;i++)
 		dest[i]=org[i];
+	
 	
 }
 static double MElement(double* A,int i, int j,int n)
@@ -92,6 +93,7 @@ void GaussElimination(int n, double* A, double* b)
 { 
 	int i,j,k;
 	int index;
+	char flag;
 	double pvt,t;
 	double f;
 	for(j=0; j<n-1;j++)
@@ -101,25 +103,35 @@ void GaussElimination(int n, double* A, double* b)
 			//Finding Pivot
 			pvt=A[MIndex(i,i,n)];
 			index=i;
+			flag=0;
 			for(k=i+1;k<n;k++)
 			{
-				if(pvt<fabs(A[MIndex(k,i,n)]))
-				{
+				if(pvt<A[MIndex(k,i,n)])
+				{	printf("pvt: %lf\n",pvt);
 					index=k;
+					flag=1;
 					pvt=fabs(A[MIndex(k,i,n)]);
 				}
 
 			}
-			MPrint(A,n);
+			//printf("%16g",pvt-0);
+			//	return;// Matrix is singular
+			
 			//Swap Rows
+		if(flag)
+		{
 			for(k=0;k<n;k++)
 			{
 				t=A[MIndex(index,k,n)];
 				A[MIndex(index,k,n)]=A[MIndex(i,k,n)];
 				A[MIndex(i,k,n)]=t;
 			}
+			#ifdef DEBUG
 			printf("Swapped\n");
 			MPrint(A,n);
+			#endif
+		}
+			
 			f=MElement(A,i,j,n) / MElement(A,j,j,n);
 			#ifdef DEBUG
 			printf("f: %lf\n",f);
@@ -156,18 +168,24 @@ int main (void)
 	double M3[2*2]={pow(10,-17),1.0,1.0,2.0};
 	double b3[2]={1.0,4.0};
 	double xV3[2];
+	double M1c[3*3],M2c[6*6],M3c[2*2];
+	double b1c[3]={3,3,-6},b2c[6]={2.5,1.5,1,1,1.5,2.5},b3c[2]={1.0,4.0};;
 	int i;
 	
+	
 	printf("part1\n");
-	GaussElimination(3,M1,b1);
-	printf("subs\n");
+	MCopy(M1,M1c,3);
+	MCopy(M2,M2c,6);
+	MCopy(M3,M3c,2);
+	
+	NaiveGaussElimination(3,M1,b1);
 	BackSubstitution(3,M1,b1,xV1);
 	for(i=0;i<3;i++)
 	{
 		printf("x%d:%16g\n",i,xV1[i]);
 	}
 	
-	GaussElimination(6,M2,b2);
+	NaiveGaussElimination(6,M2,b2);
 	printf("subs\n");
 	MPrint(M2,6);
 	BackSubstitution(6,M2,b2,xV2);
@@ -175,8 +193,35 @@ int main (void)
 	{
 		printf("x%d:%16g\n",i,xV2[i]);
 	}
-	GaussElimination(2,M3,b3);
+	
+	NaiveGaussElimination(2,M3,b3);
+	printf("subs\n");
 	BackSubstitution(2,M3,b3,xV3);
+	for(i=0;i<2;i++)
+	{
+		printf("x%d:%16g\n",i,xV3[i]);
+	}
+	printf("part2\n");
+	
+	
+	GaussElimination(3,M1c,b1c);
+	printf("subs\n");
+	BackSubstitution(3,M1c,b1c,xV1);
+	for(i=0;i<3;i++)
+	{
+		printf("x%d:%16g\n",i,xV1[i]);
+	}
+	
+	GaussElimination(6,M2c,b2c);
+	printf("subs\n");
+	BackSubstitution(6,M2c,b2c,xV2);
+	for(i=0;i<6;i++)
+	{
+		printf("x%d:%16g\n",i,xV2[i]);
+	}
+	printf("subs\n");
+	GaussElimination(2,M3c,b3c);
+	BackSubstitution(2,M3c,b3c,xV3);
 	for(i=0;i<2;i++)
 	{
 		printf("x%d:%16g\n",i,xV3[i]);
