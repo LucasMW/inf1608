b311e1a83150cda8fcb556f3c28dce559a7b66f7
LucasMW <great-lucas@hotmail.com>
2015-07-08 20:44:28 -0300

    Verlet

--- trab/source.c
+++ trab/source.c
@@ -9,10 +9,43 @@
 #define G 9.81
 #define L 10
 
-double AdaptativeRungeKutta(double t0, double y0, double h0, double t1, double (*f) (double t, double y), double emax)
+
+double VerletIntegration(double t0,double t1,double y0,double v0,double (*f) (double t, double y)) //h = timestep
+{
+	double yAnt, y, yNext;
+	double h, t;
+	h=(t1-t0)/10;
+	//initialization
+	yAnt = y0;
+	y= yAnt + h*v0;
+
+
+	for(t=t0;t<t1;t+=h)
+	{
+		yNext= y + (y - yAnt) + f(t,y) * h *h;
+
+		yAnt= y;
+		y = yNext;
+	}
+	return y;
+}
+
+double calculateW(double t,double y,double h,double w,double (*f) (double t, double y))
+{
+	double k1,k2,k3,k4;
+		k1=h*f(t,y);
+		k2=h*f(t+h/2,y+k1/2.0);
+		k3=h*f(t+h/2,y+k2/2.0);
+		k4=h*f(t+h,y+k3);
+		return w + (k1+2*(k2+k3)+k4)/6.0;
+
+}
+double AdaptativeRungeKutta(double t0, double y0, double w0, double h0, double t1, double (*f) (double t, double y), double emax)
 {
 	double y,y1,y2;
 	double s1,s2,s3,s4;
+	double k1,k2,k3,k4;
+	double w,w1,w2;
 	double t;
 	double fty;
 	double h,hNew;
@@ -21,11 +54,20 @@ double AdaptativeRungeKutta(double t0, double y0, double h0, double t1, double (
 	for(t=t0,y=y0,h=h0;t<t1;)
 	{
 		//control eval
-		s1=h*f(t,y);
-		s2=h*f(t+h/2,y+s1/2.0);
-		s3=h*f(t+h/2,y+s2/2.0);
-		s4=h*f(t+h,y+s3);
+		k1=h*f(t,y);
+		k2=h*f(t+h/2,y+k1/2.0);
+		k3=h*f(t+h/2,y+k2/2.0);
+		k4=h*f(t+h,y+k3);
+		w1 = w + (k1+2*(k2+k3)+k4)/6.0;
+
+
+		s1=h*f(t,w1);
+		s2=h*f(t+h/2,w+s1/2.0);
+		s3=h*f(t+h/2,w+s2/2.0);
+		s4=h*f(t+h,w+s3);
 		y1 = y + (s1+2*(s2+s3)+s4)/6.0;
+
+
 
 		//test eval
 		s1=h*f(t,y);
@@ -72,7 +114,7 @@ double AdaptativeRungeKutta(double t0, double y0, double h0, double t1, double (
 				#endif
 			}
 
-			hNew=h*sqrt(emax/error);
+			hNew=h*pow(emax/error,-5); //5th root  
 			if(hNew > 1.2*h)
 			{
 				hNew = 1.2*h;
@@ -109,13 +151,18 @@ double RungeKutta(double t0, double t1, double h, double y0, double (*f) (double
 }
 
 
-double FunctionSecondDerivative(double theta, double theta0)
+
+double FunctionSecondDerivative(double t, double theta)
 {
 	return -G/L * sin(theta);
 }
 double FunctionDerivative(double theta)
 {
 	return  G/L * cos(theta);
+}
+double Function(double t, double theta)
+{
+	return -G/L * sin(theta);
 }
 double FunctionRight(double t,double theta0)
 {
@@ -127,12 +174,13 @@ int main (void)
 	double theta0= M_PI/30;
 	double T = 2 * M_PI * sqrt(L /G);
 	double t;
+	double w;
 	for(t=0;t<T;t += 0.01)
 	{
-		printf("theta(%lf): %lf \n",t,FunctionRight(t,theta0));
+		printf("theta(%lf): %lf VS %lf\n",t,FunctionRight(t,theta0),VerletIntegration(0,t,theta0,0,FunctionSecondDerivative));
 
 	}
-	AdaptativeRungeKutta(0,theta0,0.01,T,FunctionSecondDerivative,pow(10,-5));
+	
 	
 	return 0;
 
